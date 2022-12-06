3c88de4cf11b059591368f24bc0c76063e2d9cc0
Socrates Mosko <119309614+socratesmosko@users.noreply.github.com>
2022-12-06 18:51:50 +0200

    Create tables.php

---
+++ tables.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Table in HTML</title>
+</head>
+<body>
+
+<table border="1">
+
+<?php
+for ( $i = 1; $i <= 25; ++$i ) 
+{ 
+  echo "<tr><td>" . $i . ". Hello, world! </td></tr> \n"; 
+} 
+?>
+</table>
+
+</body>
+</html>
